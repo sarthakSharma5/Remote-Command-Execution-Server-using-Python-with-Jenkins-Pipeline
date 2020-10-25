e03748a82adb8d6f032c2fad395618678ab71db8
sarthakSharma5 <64486079+sarthakSharma5@users.noreply.github.com>
2020-10-26 03:12:54 +0530

    update cmdapi.py 2

--- cmdapi.py
+++ cmdapi.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
+	
 
 import subprocess
 import cgi
-
+	
 print("content-type: text/html")
-print()
+print()	
 
 form = cgi.FieldStorage()
-cmd = form.getvalue("cmd")
+get_cmd = form.getvalue("cmd")	
 
-if 'sudo' in cmd:
-    cmd = cmd.replace('sudo', '')
-    print("Cannot allow use of sudo <BR>")
-
-print("command: {} | ".format(cmd))
+# cmd = "date"
+cmd = "sudo "+get_cmd
+print("command: {} | \n".format(cmd))
 output = subprocess.getstatusoutput(cmd)
-
-print("status-code: {} | ".format(output[0]))
+	
+print("status-code: {} | \n".format(output[0]))
 print()
-print("output: <PRE> {} </PRE> \n".format(output[1]))
-print("<BR>")
-
+print("output: <PRE> {} <PRE> \n".format(output[1]))
 print()
-
